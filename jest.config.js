af91d3b93bdf8f392a2a030cb040613fc5f961a2
listenrightmeow <mike@fatt.ist>
2020-08-14 12:56:33 -0700

    general cleanup

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    moduleNameMapper: {
-        "^@authorizers/(.*)$": "<rootDir>/src/authorizers/$1",
-        "^@helpers/(.*)$": "<rootDir>/src/helpers/$1",
-        "^@routes/(.*)$": "<rootDir>/src/routes/$1",
-        "^@services/(.*)$": "<rootDir>/src/services/$1"
-    }
-}
